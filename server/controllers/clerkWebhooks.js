420a397125273fc44e2a4ef0b85e12a6006b9169
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-07 17:55:41 +0530

    webhooks fix1

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -21,10 +21,10 @@ const clerkWebhooks = async (req, res)=> {
     const {data, type} = req.body
 
     const userData = {
-        _id: data._id,
-        email: data.email_addresses[0].email_address,
-        username: data.first_name + " " + data.last_name,
-        image: data.image_url
+        _id: data.id,
+        email: data.email_addresses?.[0]?.email_address || "theflash.tfbro@gmail.com",
+        username: `${data.first_name ?? ""} ${data.last_name ?? ""}`.trim() || "No Name",
+        image: data.image_url || "",
     }
 
     switch (type) {
