9cebfd04f1d33712a5e16b915a0d0506661095d6
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-07 17:55:41 +0530

    webhooks fix1

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 
 const UserSchema = mongoose.Schema({
     _id: { type: String, required: true },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    image: { type: String, required: true },
+    username: { type: String },
+    email: { type: String },
+    image: { type: String },
     role: { type: String, enum: ["user", "hotelOwner"], default: "user"},
-    recentSearchedCities: [{type: String, required: true}]
+    recentSearchedCities: [{type: String}]
 }, {timestamps: true})
 
 const User = mongoose.model("User", UserSchema)
