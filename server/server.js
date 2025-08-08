f1518e072af46187489d5155a591c18082b99cdc
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-09 02:20:15 +0530

    webhook fix2

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
+// server.js
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from './controllers/clerkWebhooks.js'
-
+import { clerkMiddleware } from "@clerk/express";
+import bodyParser from "body-parser";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
 
 const app = express();
-app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(cors());
+app.use(clerkMiddleware());
 
-// Middleware
-app.use(express.json())
-app.use(clerkMiddleware())
-
-// API to listen to Clerk Webhooks
-app.use('/api/clerk', clerkWebhooks)
+// Raw body for Clerk Webhooks
+app.post("/api/clerk", bodyParser.raw({ type: "application/json" }), clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is Working"));
 
