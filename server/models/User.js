6b3eecdade78b16d10e41a73bda0d632978d678a
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-16 00:23:47 +0530

    MongoDB data store fix

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
     image: { type: String, required: true },
-    role: { type: String, enum: ["user", "hotelOwner"], default: "user"},
-    recentSearchedCities: [{type: String, required: true}]
-}, {timestamps: true})
+    role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+    recentSearchedCities: {
+        type: [String],
+        default: []
+    }
+}, { timestamps: true });
 
-const User = mongoose.model("User", UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-export default User
+export default User;
