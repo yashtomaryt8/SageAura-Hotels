98afce6c7650f495d47ee16ee2b8ad4ba7700c2f
RakhiRawat064 <rakhirawat064@gmail.com>
2025-08-16 00:23:47 +0530

    MongoDB data store fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,34 +1,28 @@
-// GET /api/user
-
+// controllers/userController.js
 export const getUserData = async (req, res) => {
-    try{
-        const role = req.user.role
-        const recentSearchedCities = req.user.recentSearchedCities
-        res.json({success: true, role, recentSearchedCities})
-    }
-    catch (error) {
-        res.json({success: false, message: error.message})
-    }
-}
+  try {
+    const { role, recentSearchedCities } = req.user;
+    res.json({ success: true, role, recentSearchedCities });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-// Store User Recent Searched Cities
+export const storeRecentSearchedCities = async (req, res) => {
+  try {
+    const { recentSearchedCity } = req.body;
+    const user = req.user;
 
-export const storeRecentSearchedCities = async ()=>{
-    try{
-        const {recentSearchedCity} = req.body
-        const user = await req.user
+    if (user.recentSearchedCities.length < 3) {
+      user.recentSearchedCities.push(recentSearchedCity);
+    } else {
+      user.recentSearchedCities.shift();
+      user.recentSearchedCities.push(recentSearchedCity);
+    }
 
-        if(user.recentSearchedCities.lenth < 3){
-            user.recentSearchedCities.push(recentSearchedCity)
-        }
-        else{
-            user.recentSearchedCities.shift()
-            user.recentSearchedCities.push(recentSearchedCity)
-        }
-        await user.save()
-        res.json({success : true, messsage : 'City added'})
-    }
-    catch (error){
-        res.json({success: false, message: error.message})
-    }
-}
+    await user.save();
+    res.json({ success: true, message: 'City added' });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
