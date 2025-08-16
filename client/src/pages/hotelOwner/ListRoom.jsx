5a5b00b8365a72b1eaa7da1372b5feb91431962d
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-16 17:27:11 +0530

    ListRoom Fixed with Toggle Availability

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-import { roomsDummyData } from './../../assets/assets';
+import React, {useState, useEffect} from 'react'
 import Title from './../../components/Title';
+import { useAppContext } from '../../context/AppContext'
+import { toast } from 'react-hot-toast';
 
 const ListRoom = () => {
-    const [rooms] = React.useState(roomsDummyData); 
+    const [rooms, setRooms] = useState([]) 
+    const {axios, getToken, user} = useAppContext()
+
+    //  Fetch Rooms of the Hotel Owner
+    const fetchRooms = async ()=>{
+        try{
+            const { data } = await axios.get('/api/rooms/owner', {headers: {Authorization: `Bearer ${await getToken()}`}})
+            if (data.success){
+                setRooms(data.rooms)
+            }
+            else{
+                toast.error(data.message)
+            }
+        }
+        catch(error){
+            toast.error(error.message)
+        }
+    }
+
+    // Toggle Availability of the Room
+    const toggleAvailability = async (roomId) =>{
+        const { data } = await axios.post('/api/rooms/toggle-availability', {roomId}, {headers: {Authorization: `Bearer ${await getToken()}`}})
+        if(data.success){
+            toast.success(data.message)
+            fetchRooms()
+        }
+        else{
+            toast.error(data.message)
+        }
+    }
+
+    useEffect(()=>{
+        if(user){
+            fetchRooms()
+        }
+    }, [user])
+
+
   return (
     <div>
       <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.'/>
@@ -31,11 +69,11 @@ const ListRoom = () => {
                             {item.pricePerNight}
                         </td>
                         <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-red-500 text-center'> 
-                            <label htmlFor="" className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3'>
-                                <input type="checkbox" className='sr-only peer' checked = {item.isAvailable}/>
-                                <div className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200'></div>
-                                <span className='dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'></span>
-                            </label>
+                            <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+                                <input onChange={()=> toggleAvailability(item._id)} type='checkbox' className='sr-only peer' checked={item.isAvailable} />
+                                <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
+                                    <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                                </label>
                         </td>
                     </tr>
                 ))}
