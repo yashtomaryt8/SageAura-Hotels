025ded1fc0c9132f755d1b99a5eccb0f47875e0f
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-16 17:27:11 +0530

    ListRoom Fixed with Toggle Availability

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -48,7 +48,7 @@ export const getRooms = async (req, res)=>{
 }
 export const getOwnerRooms = async (req, res)=>{
     try{
-        const hotelData = await Hotel({owner: req.auth.userId})
+        const hotelData = await Hotel.findOne({owner: req.auth.userId})
         const rooms = await Room.find({hotel: hotelData._id.toString()}).populate('hotel')
         res.json({success: true, rooms})
     }
