6333a30d525ee42b657ae1022336978db7e4b18a
Yash Tomar <yashtomar.yt8@gmail.com>
2025-08-18 04:02:49 +0530

    Time to deploy Project completed

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -20,18 +20,25 @@ const clerkWebhooks = async (req, res)=> {
 
     const {data, type} = req.body
 
-    const userData = {
+    
+
+    switch (type) {
+      case "user.created":{
+        const userData = {
         _id: data.id,
         email: data.email_addresses[0].email_address,
         username: data.first_name + " " + data.last_name,
         image: data.image_url
     }
-
-    switch (type) {
-      case "user.created":{
         await User.create(userData)
         break}
       case "user.updated":{
+        const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        username: data.first_name + " " + data.last_name,
+        image: data.image_url
+    }
         await User.findByIdAndUpdate(data.id, userData, { new: true, upsert: true })
         break}
       case "user.deleted":{
